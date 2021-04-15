fe53abb369a4269267e7205ea816323b1d16a112
KishanGururaj <81949579+KishanGururaj@users.noreply.github.com>
2021-04-16 00:01:29 +0530

    Adding:1st commit

---
+++ garage-webservices/src/main/java/com/wolken/wolkenapp/controller/GarageController.java
@@ -0,0 +1,49 @@
+package com.wolken.wolkenapp.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wolken.wolkenapp.dto.VehicleDTO;
+
+@RestController
+@RequestMapping("/")
+public class GarageController {
+	VehicleDTO dto=new VehicleDTO();
+	public GarageController() {
+		dto.setId(1);
+		dto.setType("car");
+		dto.setNoOfWheels(4);
+		dto.setColor("red");
+	}
+	@GetMapping("/getAll")
+	public VehicleDTO getAll() {
+		return dto;
+	}
+	@PostMapping("/save")
+	public String save() {
+		return "data saved";
+	}
+	/*
+	 * @PutMapping
+	 * 
+	 * @DeleteMapping
+	 */
+	@PutMapping("/update")
+	public String update(@RequestBody VehicleDTO dto)
+	{
+		return "updated "+ dto.getType()+" "+ dto.getColor();
+	}
+
+	@DeleteMapping("/delete")
+	public String del(@RequestBody VehicleDTO dto)
+	{
+		return "deleted " + dto.getType()+" "+ dto.getColor();
+	}
+
+}
+
---
+++ garage-webservices/src/main/java/com/wolken/wolkenapp/dto/VehicleDTO.java
@@ -0,0 +1,15 @@
+package com.wolken.wolkenapp.dto;
+
+import lombok.Data;
+
+@Data
+public class VehicleDTO {
+	
+
+
+		private int id;
+		private String type;
+		private int noOfWheels;
+		private String color;
+
+}
